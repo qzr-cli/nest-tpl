ffcf76b341feb0ca4c6581ecb1e5dc07054b683e
邱卓然 <z5021996@vip.qq.com>
2021-12-16 10:55:31 +0800

    feat(development): :tada: init

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/**
+ * @Date         : 2021-12-15 16:59:07
+ * @Description  : eslint
+ * @Autor        : Qzr(z5021996@vip.qq.com)
+ * @LastEditors  : Qzr(z5021996@vip.qq.com)
+ * @LastEditTime : 2021-12-16 10:46:25
+ */
+
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    '@constq/eslint-config-qzr/index',
+    '@constq/eslint-config-qzr/typescript',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
