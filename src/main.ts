2a5a42d258c92b7848c116001fffbeb9c389d163
邱卓然 <z5021996@vip.qq.com>
2021-12-16 14:22:50 +0800

    feat(development): 添加文档

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.setGlobalPrefix('api') // 设置全局路由前缀
+
+  await app.listen(7001)
 }
 bootstrap()
